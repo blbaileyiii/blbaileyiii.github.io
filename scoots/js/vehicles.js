2a9d15f2b45f829d0337a7af7d372aa8d8949ca7
iii <blbaileyiii.naa@gmail.com>
2020-12-17 15:02:29 -0800

    home page - adding overlay

--- scoots/js/vehicles.js
+++ scoots/js/vehicles.js
@@ -1,6 +1,7 @@
 const requestURL = 'data/price.json';
 
-fetch(requestURL)
+function mainServices() {
+    fetch(requestURL)
     .then(function (response) {
         return response.json();
     })
@@ -22,18 +23,24 @@ fetch(requestURL)
             let div = document.createElement('div');
             let img = document.createElement('img');
             let divImg = document.createElement('div');
+            let divOverlayP = document.createElement('div');
+            let divOverlay = document.createElement('div');
             let divTxt = document.createElement('div');
             let divBtn = document.createElement('div');
             let a = document.createElement('a');
 
             cardItem.classList.add('card-item');
             divImg.classList.add('center');
+            divOverlayP.classList.add('overlay-parent')
+            divOverlay.classList.add('overlay');
             divTxt.classList.add('text');
             divBtn.classList.add('button');
 
             h4.textContent = vehicles[i].type + "s";
             img.src = "images/" + vehicles[i].defaultImg;
             img.alt = vehicles[i].type;
+
+            divOverlay.textContent = "$" + vehicles[i].model[0].booking[0].full;
 
             divTxt.innerHTML = "<h5>Models:</h5>";
             for (let ii in vehicles[i].model) {
@@ -47,9 +54,14 @@ fetch(requestURL)
             a.href = "reservation.html";
             a.textContent = "Reserve your " + vehicles[i].type + " today!";
 
+            
 
-            divBtn.append(a);
-            divImg.append(img);
+            divBtn.append(a);  
+
+            divImg.append(divOverlayP);
+
+            divOverlayP.append(img);
+            divOverlayP.append(divOverlay);
 
             div.append(divImg);
             div.append(divTxt);
@@ -63,6 +75,4 @@ fetch(requestURL)
         }
         
     });
-
-/* Fonts from Google Fonts - more at https://fonts.google.com */
-WebFont.load({google: {families: ["Cinzel", "Raleway"]}});
+}
